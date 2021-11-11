8a9ae089888a7911f18b6d45693d5e8cb963b4bc
Pablo Martinez <pablomartinezsepulveda@hotmail.com>
2021-11-11 17:57:26 -0300

    Se crearon controladores de product y categorias. Se logró display de categoría en index products

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -6,8 +6,11 @@ ActiveAdmin.register Product do
   # Permite contenerlo en el menú adminstración
   menu parent: :some_menu_id
 
+
   permit_params :name, :description, :price, :discount_price, :active, :store_id, :image, category_ids:[],
   categories_attributes: [:name]
+  # permit_params :name, :description, :price, :discount_price, :active, :store_id, :image,
+  # product_categories: [:id, :product_id, :category_id, :_destroy, :name]
 
 
   filter :name
@@ -15,14 +18,44 @@ ActiveAdmin.register Product do
   filter :price, as: :numeric_range_filter
   filter :active
 
+  # Controllador Product
+      controller do
+    
+        before_action :find_store, only: [:new, :create]
 
+        def new
+          @product = Product.new
+        end
+
+        def create
+          @product = Product.new(product_params)
+          @product.store = @store
+          @product.category_ids = params[:product][:category_ids]
+          if @product.save
+            redirect_to admin_products_path, notice: "Producto creado con éxito"
+          end
+        end
+    
+        private
+    
+        def product_params
+          params.require(:product).permit(:name, :description, :price, :discount_price, :active, :store_id, :image)
+        end
+    
+        def find_store
+          @store = Store.first(params[:store_id])
+        end
+    
+      end
+
+    # Index de products
   index do
     selectable_column
     column :name
     column :description
     number_column :price , as: :currency, unit: "$", separator: "."
-    column 'Categoría' do |display|
-      display.categories.name
+    column 'Categoría' do |product|
+      product.categories.map{ |bg| bg.name}
     end
     toggle_bool_column :active
     column "Image" do |product|
@@ -35,36 +68,7 @@ ActiveAdmin.register Product do
     actions
   end
 
-
+  # Render de New Product
   form partial: 'form', locals: {resource: Product.new}
-
-  controller do
-
-    before_action :find_store, only: [:new, :create]
-
-    def new
-      @product = Product.new
-    end
-
-    def create
-      @product = Product.new(product_params)
-      @product.store = @store
-      @product.category_ids = params[:product][:category_ids]
-      if @product.save
-        redirect_to admin_products_path, notice: "Producto creado con éxito"
-      end
-    end
-
-    private
-
-    def product_params
-      params.require(:product).permit(:name, :description, :price, :discount_price, :active, :store_id, :image)
-    end
-
-    def find_store
-      @store = Store.first(params[:store_id])
-    end
-
-  end
 
 end
