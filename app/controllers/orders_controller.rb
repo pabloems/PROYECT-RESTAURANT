9f37ba5f6b2ecf44ac14364e66e4d10776737c3e
Pablo Martinez <pablomartinezsepulveda@hotmail.com>
2021-11-20 00:25:17 -0300

    before install stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,35 @@
+require_relative '../models/cart'
 class OrdersController < ApplicationController
+
+  before_action :set_order, only: [:show]
+
+  def index
+    @orders = current_user.orders
+  end
+
+  def show
+  end
+
+  def create
+    @order = Order.new
+    @order.user = current_user
+    @order.cart = current_cart
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+
 end
