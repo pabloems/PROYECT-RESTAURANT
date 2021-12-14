f4f1f68b17ad1b4c4e7b84e29f9e773591c4c406
Pablo Martinez <pablomartinezsepulveda@hotmail.com>
2021-12-14 02:07:53 -0300

    try-stripe

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,6 +1,9 @@
 class StripeCheckoutSessionService
   def call(event)
+    @order.user = current_user
+    @order.cart = current_cart
+    current_cart = Cart.find_by_id(session[:cart_id])
     order = Order.find_by(checkout_session_id: event.data.object.id)
-    order.update(status: 'Pagado')
+    order.update(status: 'Pagado', cart: current_cart, user: current_user)
   end
 end
