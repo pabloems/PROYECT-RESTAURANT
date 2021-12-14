75ffe5cf3ec0f0ad72bf7a6f706a2ec1849fb0ee
Pablo Martinez <pablomartinezsepulveda@hotmail.com>
2021-12-14 20:52:38 -0300

    .

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -3,8 +3,7 @@ class StripeCheckoutSessionService
 
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(status: 'Pagado')
-    @order.save
     current_cart.destroy
-    session[:cart_id] = nil
+
   end
 end
