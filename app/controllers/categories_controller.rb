04c1c543f3300214f16bb371d690bd5257b1b9a1
Pablo Martinez <pablomartinezsepulveda@hotmail.com>
2021-12-22 03:35:23 -0300

    card product style basic, include image

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,7 +8,7 @@ class CategoriesController < ApplicationController
       @pagy, @products = pagy(@q.result(distinct: true), items: 5)
       @q = Product.ransack(params[:q])
     else
-    @pagy, @products = pagy(@category.products.visible, items: 5)
+    @pagy, @products = pagy(@category.products.visible, items: 6)
     end
     @cart_item = current_cart.cart_items.new
   end
