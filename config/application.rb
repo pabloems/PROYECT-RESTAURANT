f6c71b86d31d35c2884236fe47ca5a40ca1ee064
Pablo Martinez <pablomartinezsepulveda@hotmail.com>
2021-12-24 02:37:35 -0300

    try

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,7 @@ module PROYECTRestaurant
     # Rails.application.config.assets.configure do |env|
     #   env.export_concurrent = false
     # end
-      config.assets.precompile += %w( active_admin.css active_admin.js)
+      # config.assets.precompile += %w( active_admin.css.scss active_admin.js)
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
